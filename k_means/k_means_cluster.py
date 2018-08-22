6983ef38179ef48ae0453979774f78b16d67ae37
Abraham Albert <abrahamalbert18@icloud.com>
2018-08-22 22:25:58 +1000

    Add files via upload

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,8 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,14 +59,23 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+exercised_stock_options =[]
+salary = []
+for f1, f2,_ in finance_features:
+    if f2!=0: 
+        exercised_stock_options.append(f2)
+    if f1!=0:
+        salary.append(f1)    
+    plt.scatter(f1 ,f2)
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans
 
+kmeans = KMeans(n_clusters=2).fit(finance_features)
+pred = list(kmeans.labels_)
 
 
 ### rename the "name" parameter when you change the number of features
@@ -74,3 +84,17 @@ try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
+
+print "Salary ranges between", min(salary),"and",max(salary)
+print "Exercised stock options ranges between", min(exercised_stock_options),"and", max(exercised_stock_options)
+
+
+from sklearn.preprocessing import MinMaxScaler
+salary.append(200000)
+exercised_stock_options.append(1000000)
+scaler = MinMaxScaler()
+rescaled_salary = scaler.fit_transform(numpy.array(salary))
+rescaled_exercised_stock_options = scaler.fit_transform(numpy.array(exercised_stock_options))
+
+print "Rescaled Salary : \n-----------------",rescaled_salary
+print "\n------------\nRescaled Exercised stock options : \n---------", rescaled_exercised_stock_options
